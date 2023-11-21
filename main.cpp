9f36553729e23a24ef316afbfeed47b2b04727c1
Grady <gradybaum05@gmail.com>
2023-11-21 17:33:57 -0500

    removed old code copy files to replace with C++

--- main.cpp
+++ main.cpp
@@ -15,14 +15,19 @@ private:
     bool touch = false;
 
 public:
+    void MainMenu();
     void Start();
+    void Statistics();
+    void Return();
+    void Rules();
+    void Credits();
     void Rectangle(float a, float b, float c, float d);
     int main();
 } Instance;
 
 /*RETURN BUTTON*/
 
-void game::Start()
+void game::MainMenu()
 {
 
     /*Start Rectangle*/
@@ -65,44 +70,21 @@ void game::Start()
         {
             if (x > x1 && x < x1 + x_w && y > y1 && y < y1 + y_w)
             {
-                /*Get rid of rectangles*/
-                LCD.Clear();
-
-                /*Say play game*/
-                LCD.SetFontColor(RED);
-                LCD.WriteAt("Play Game Here", 88, 88);
-
-                LCD.SetFontColor(GRAY);
-                Rectangle(200, 200, x_return_w, y_return_w);
-                LCD.SetFontColor(WHITE);
-                LCD.WriteAt("Return", 205, 204);
+                Start();
                 touch = true;
             }
 
             else if (x > x2 && x < x2 + x_w && y > y2 && y < y2 + y_w)
             {
-                /*Get rid of rectangles*/
-                LCD.Clear();
-
-                /*Show Statistics*/
-                LCD.SetFontColor(GREEN);
-                LCD.WriteRC("Longest survival time: ", 3, 1);
-                LCD.WriteRC("Distance Travelled: ", 5, 1);
+                Statistics();
             }
             else if (x > x3 && x < x3 + x_w && y > y3 && y < y3 + y_w)
             {
-                /*Get rid of rectangles*/
-                LCD.Clear(BLACK);
-                touch = true;
+                Rules();
             }
             else if (x > x4 && x < x4 + x_w && y > y4 && y < y4 + y_w)
             {
-                /*Get rid of rectangles*/
-                LCD.Clear();
-
-                LCD.SetFontColor(MAGENTA);
-                LCD.WriteAt("Grady Baum", 102, 120);
-                LCD.WriteAt("Adam Ballas", 97, 80);
+                Credits();
             }
         }
     }
@@ -115,7 +97,7 @@ int main()
     LCD.SetBackgroundColor(BLACK);
     LCD.Clear();
 
-    mine.Start();
+    mine.MainMenu();
 
     while (1)
     {
@@ -126,8 +108,74 @@ int main()
 }
 
 /*Easier rectangles*/
-void game::Rectangle(float a, float b, float c, float d)
+void game::Rectangle(float startX, float startY, float width, float height)
 {
-    LCD.DrawRectangle(a, b, c, d);
-    LCD.FillRectangle(a, b, c, d);
+    LCD.DrawRectangle(startX, startY, width, height);
+    LCD.FillRectangle(startX, startY, width, height);
+}
+
+/*They click start*/
+void game::Start()
+{
+    /*Get rid of rectangles*/
+    LCD.Clear();
+
+    /*Say play game*/
+    LCD.SetFontColor(RED);
+    LCD.WriteAt("Play Game Here", 88, 88);
+    Return();
+    
+}
+/*They click start*/
+void game::Statistics()
+{
+    /*Get rid of rectangles*/
+    LCD.Clear();
+
+    /*Show Statistics*/
+    LCD.SetFontColor(GREEN);
+    LCD.WriteRC("Longest survival time: ", 3, 1);
+    LCD.WriteRC("Distance Travelled: ", 5, 1);
+
+    // wait for return
+    Return();
+}
+
+void game::Rules()
+{
+    LCD.Clear();
+
+    Return();
+}
+void game::Credits()
+{
+    /*Get rid of rectangles*/
+    LCD.Clear();
+
+    LCD.SetFontColor(MAGENTA);
+    LCD.WriteAt("Grady Baum", 102, 120);
+    LCD.WriteAt("Adam Ballas", 97, 80);
+
+    Return();
+}
+void game::Return()
+{
+    LCD.SetFontColor(GRAY);
+    Rectangle(200, 200, x_return_w, y_return_w);
+    LCD.SetFontColor(WHITE);
+    LCD.WriteAt("Return", 205, 204);
+    bool waitingForReturn = true;
+
+    // wait for return button to be pressed
+    while (waitingForReturn)
+    {
+        while (LCD.Touch(&x, &y))
+        {
+            if (x > 200 && x < 200 + x_return_w && y > 200 && y < 200 + y_return_w)
+            {
+                MainMenu();
+                waitingForReturn = false;
+            }
+        }
+    }
 }
