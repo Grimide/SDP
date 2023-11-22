03e9666b18569a4e8562fb69370eb5cb2cd87bbf
Grady <gradybaum05@gmail.com>
2023-11-22 15:39:19 -0500

    Most recent version, need to fix death

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ class game
 private:
     float x1 = 5, y1 = 20, y2 = 20, y3 = 130, x2 = 190, x3 = 5, x, y;
     float x4 = 190, y4 = 130;
-    float i = 0, k = 0;
+    float i = 0;
     float speed = 0.2;
+    float y_character = 210;
+    float car_y[6];
     float car_x[6] = {0, 129, 204, 172, 33, 239};
     float car_speed[6] = {5, 4, 6, 7, 8, 2};
     float x_return_w = 80, y_return_w = 20;
@@ -131,30 +133,40 @@ void game::Start()
         while (!LCD.Touch(&x, &y))
         {
             Refresh();
+            /*If for death does not work, commenting it out*/
+            /*if (((car_x[0] + 25 <= 184 && car_x[0] + 25 >= 134) && (y_character > car_y[0] && y_character < (car_y[0] + 15))) 
+            || ((car_x[1] + 25 >= 184 && car_x[1] + 25 >= 134) && (y_character > car_y[1] && y_character < (car_y[1] + 15))) 
+            || ((car_x[2] + 25 >= 184 && car_x[2] + 25 >= 134) && (y_character > car_y[2] && y_character < (car_y[2] + 15))) 
+            || ((car_x[3] + 25 >= 184 && car_x[3] + 25 >= 134) && (y_character > car_y[3] && y_character < (car_y[3] + 15))) 
+            || ((car_x[4] + 25 >= 184 && car_x[4] + 25 >= 134) && (y_character > car_y[4] && y_character < (car_y[4] + 15))) 
+            || ((car_x[5] + 25 >= 184 && car_x[5] + 25 >= 134) && (y_character > car_y[5] && y_character < (car_y[5] + 15))))
+            {
+                dead = true;
+            }*/
         }
 
         while (LCD.Touch(&x, &y))
         {
-            if (!color) // Correctly assigns color to replace previous character
-            {
-                LCD.SetFontColor(GREEN);
-                color = true;
-            }
-            else if (color)
-            {
-                LCD.SetFontColor(GRAY);
-                color = false;
-            }
-            LCD.DrawCircle(159, k + 190, 9); // delete character
-            k -= 20;
-            LCD.SetFontColor(BLUE);
-            LCD.DrawCircle(159, k + 19, 9); // new character
-
-            Refresh();
         }
-        Sleep(.05);
+        if (!color) // Correctly assigns color to replace previous character
+        {
+            LCD.SetFontColor(GREEN);
+            color = true;
+        }
+        else if (color)
+        {
+            LCD.SetFontColor(GRAY);
+            color = false;
+        }
+        LCD.DrawCircle(159, y_character, 9); // delete character
+        y_character -= 20;
+        LCD.SetFontColor(BLUE);
+        LCD.DrawCircle(159, y_character, 9); // new character
+        int n;
+        Refresh();
     }
     /*Say play game*/
+    LCD.Clear();
     LCD.SetFontColor(RED);
     LCD.WriteAt("Play Game Here", 88, 88);
     Return();
@@ -163,96 +175,109 @@ void game::Start()
 /*moves the screen down*/
 void game::Refresh()
 {
+    int n;
+    for (n = 0; n < 5; n++)
+    {
+        car_y[n + 1] = i + (42.5 + 40 * n);
+    }
+    car_y[0] = {i};
 
-/*builds map initially*/
+    /*builds map initially*/
+
     LCD.SetFontColor(GRAY);
     Rectangle(0, i, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[0], i, 25, 15);
+    Rectangle(car_x[0], car_y[0], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 22.5, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 40, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[1], i + 42.5, 25, 15);
+    Rectangle(car_x[1], car_y[1], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 60, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 80, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[2], i + 82.5, 25, 15);
+    Rectangle(car_x[2], car_y[2], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 100, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 120, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[3], i + 122.5, 25, 15);
+    Rectangle(car_x[3], car_y[3], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 140, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 160, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[4], i + 162.5, 25, 15);
+    Rectangle(car_x[4], car_y[4], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 180, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 200, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[5], i + 202.5, 25, 15);
+    Rectangle(car_x[5], car_y[5], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 220, 319, 20);
 
     LCD.SetFontColor(BLUE);
-    LCD.DrawCircle(159, k + 190, 9);
+    LCD.DrawCircle(159, y_character, 9);
 
-    i += speed; //Updates location of grass+roads
-    k += speed; //Updates location of character
+    i += speed;           // Updates location of grass+roads
+    y_character += speed; // Updates location of character
     int j;
 
     for (j = 0; j < 6; j++)
     {
-        car_x[j] += car_speed[j]; //Updates car location
+        car_x[j] += car_speed[j]; // Updates car location
     }
+
+    for (n = 0; n < 5; n++)
+    {
+        car_y[n + 1] = i + (42.5 + 40 * n);
+    }
+    car_y[0] = {i};
 
     LCD.SetFontColor(GRAY);
     Rectangle(0, i, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[0], i, 25, 15);
+    Rectangle(car_x[0], car_y[0], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 22.5, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 40, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[1], i + 42.5, 25, 15);
+    Rectangle(car_x[1], car_y[1], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 60, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 80, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[2], i + 82.5, 25, 15);
+    Rectangle(car_x[2], car_y[2], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 100, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 120, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[3], i + 122.5, 25, 15);
+    Rectangle(car_x[3], car_y[3], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 140, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 160, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[4], i + 162.5, 25, 15);
+    Rectangle(car_x[4], car_y[4], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 180, 319, 20);
     LCD.SetFontColor(GRAY);
     Rectangle(0, i + 200, 319, 20);
     LCD.SetFontColor(RED);
-    Rectangle(car_x[5], i + 202.5, 25, 15);
+    Rectangle(car_x[5], car_y[5], 25, 15);
     LCD.SetFontColor(GREEN);
     Rectangle(0, i + 220, 319, 20);
 
     LCD.SetFontColor(BLUE);
-    LCD.DrawCircle(159, k + 190, 9);
+    LCD.DrawCircle(159, y_character, 9);
 }
 
 /*They click Statistics*/
